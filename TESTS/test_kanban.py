76dd478e198a398a538fedac75f4127aac340fb2
roe rho <ronan.binmohdhafizchai@study.hs-duesseldorf.de>
2025-12-10 15:44:56 +0100

    Generate task and columns work

--- TESTS/test_kanban.py
+++ TESTS/test_kanban.py
@@ -1,4 +1,9 @@
 import unittest
+import threading
+import time
+import Kanban as K
+from Kanban import *
+
 
 
 class TestColumnGeneration(unittest.TestCase):
@@ -6,16 +11,26 @@ class TestColumnGeneration(unittest.TestCase):
 
     def test_generate_columns(self):
         """Test column generation."""
-        pass
-
+        result = generate_columns(5)
+        self.assertEqual(len(result),5)
+        self.assertTrue(all(isinstance(col,list) for col in result))
+        self.assertTrue(all(len(col)==0 for col in result))
 
 class TestTaskGeneration(unittest.TestCase):
     """Test cases for generate_task function."""
 
     def test_task_generation(self):
         """Test task generation."""
-        pass
-
+        global running
+        running = True
+        K.generate_columns(3)
+        initial_backlog_length = len(K.column[0])
+        task_thread = threading.Thread(target = K.generate_task) #has to call the generate task from kanban
+        task_thread.daemon = True
+        task_thread.start()
+        time.sleep(4)
+        running = False
+        self.assertGreater(len(K.column[0]),initial_backlog_length)
 
 class TestTaskProcessing(unittest.TestCase):
     """Test cases for process_tasks function."""
