e1b51c9809fb01a314abff7bad9a97a0ce5fca61
roe rho <ronan.binmohdhafizchai@study.hs-duesseldorf.de>
2025-12-16 14:36:04 +0100

    JS script for start stop reset buttons

--- Frontend/script.js
+++ Frontend/script.js
@@ -6,4 +6,26 @@ let isRunning = false;
 
 startBtn.addEventListener('click', function() {
     isRunning = true;
-    console.log()
+    console.log('Kanban simulation started');
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', function() {
+    isRunning = false;
+    console.log('Kanban simulation stopped');
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+resetBtn.addEventListener('click', function() {
+    isRunning = false;
+    console.log('Kanban simulation reset');
+    document.querySelectorAll('.cards').forEach(column => {
+        column.innerHTML = '';
+    });
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+stopBtn.disabled = true;
